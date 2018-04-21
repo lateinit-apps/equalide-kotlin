aad9c257a5785056152cb7f415873b44387d767b
Artem Bondar <artem.bondar.dev@gmail.com>
2018-04-21 20:49:46 +0300

    Deleted action button and text field

--- app/src/main/java/equalide/kotlin/MainView.kt
+++ app/src/main/java/equalide/kotlin/MainView.kt
@@ -1,7 +1,6 @@
 package equalide.kotlin
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -17,11 +16,6 @@ class MainView : AppCompatActivity(), NavigationView.OnNavigationItemSelectedLis
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_view)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
