17b3f56d2a15485d534e8e0187ff8b5565ee4529
Artem Bondar <artem.bondar.dev@gmail.com>
2018-04-28 23:31:49 +0300

    Changed .xml naming. Added new layout files.

--- app/src/main/java/equalide/kotlin/MainView.kt
+++ app/src/main/java/equalide/kotlin/MainView.kt
@@ -35,7 +35,7 @@ class MainView : AppCompatActivity(), NavigationView.OnNavigationItemSelectedLis
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main_view, menu)
+        menuInflater.inflate(R.menu.actionbar, menu)
         return true
     }
 
