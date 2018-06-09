950d13438fa0798cf92f3ef67ecefa991097a5d2
Artem Bondar <artem.bondar.dev@gmail.com>
2018-06-09 16:27:58 +0300

    Added subject to mail, created release build configuration

--- app/src/main/java/equalide/kotlin/activities/Main.kt
+++ app/src/main/java/equalide/kotlin/activities/Main.kt
@@ -170,6 +170,7 @@ class Main : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
         val intent = Intent(Intent.ACTION_SENDTO)
         intent.data = Uri.parse("mailto:${resources
             .getString(R.string.contact_mail_address)}")
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Equalide")
 
         // Get list of all installed mail clients on device that are not disabled
         val appList = this.packageManager.queryIntentActivities(intent,
