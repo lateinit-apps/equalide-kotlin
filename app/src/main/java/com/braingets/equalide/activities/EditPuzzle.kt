80b24b154125efc6e9c14488fb10727b75e1a706
Artem Bondar <artem.bondar.dev@gmail.com>
2018-07-25 19:15:26 +0300

    Provided action bar for puzzle edit screen

--- app/src/main/java/com/braingets/equalide/activities/EditPuzzle.kt
+++ app/src/main/java/com/braingets/equalide/activities/EditPuzzle.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 
 import android.support.v7.app.AppCompatActivity
 
+import android.view.Menu
+
 import kotlinx.android.synthetic.main.main_screen.*
 
 import com.braingets.equalide.R
@@ -16,5 +18,10 @@ class EditPuzzle : AppCompatActivity() {
         setContentView(R.layout.activity_edit_puzzle)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.edit_puzzle_actionbar_menu, menu)
+        return true
     }
 }
